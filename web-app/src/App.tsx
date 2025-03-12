99d84bc0de47c5882aef2b90d3b20898070a99db
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-12 22:17:41 +0800

    q

---
+++ web-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    
+<div className="text-3xl font-bold underline text-blue-500">
+      Hello, TailwindCSS!
+
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
