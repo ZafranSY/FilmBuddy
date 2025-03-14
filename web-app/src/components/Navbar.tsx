a2e6fbe29f02bda4a55699f7bb42fc5182900a14
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-15 00:14:40 +0800

    Update Navbar.tsx

--- web-app/src/components/Navbar.tsx
+++ web-app/src/components/Navbar.tsx
@@ -3,15 +3,22 @@ import React from 'react'
 function Navbar() {
   return (
     <nav className='bg-white borfer-gray-200 dark-bg-gray-900'>
-      <div className="max-w-screen-xl flex  flex-wrap items-center justify-center">
-
-        <div className="items-center">
-          <ul>
-            <li>
-              movie list
+      <div className="max-w-screen-xl flex  flex-wrap items-center justify-between mx-auto p-4 ">
+         {/* Logo */}
+         <div className="flex-shrink-0">
+          <h1 className="text-xl font-bold text-gray-900  no-underline ">Logo</h1>
+        </div>
+        <div className="flex-grow items-center justify-center ">
+          <ul className='flex items-center justify-center space-x-4 w-full text-black no-underline'>
+            <li className='no-underline'>
+            <a href="#" className="no-underline  font-semibold">
+                movie list
+              </a>
             </li>
-            <li>
-              movie Vibe
+            <li className='no-underline'>
+            <a href="#" className="no-underline hover:underline font-semibold">
+                movie vibe
+              </a>
             </li>
           </ul>
         </div>
