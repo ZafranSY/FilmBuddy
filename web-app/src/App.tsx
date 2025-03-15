5fc19c4c39d8beb57272c3f30354755b3c509e92
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-15 18:04:25 +0800

    Update App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
 import MovieHub from './components/MovieHub';
 import MoodMovie from './components/MoodMovie';
+import Login from './components/login';
 function App() {
   return (
     
@@ -16,6 +17,7 @@ function App() {
         <Route path="/">
           <Route path='/MovieHub' element={<MovieHub/>}/>
           <Route path='/MoodMovie' element={<MoodMovie/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
