e5a1b10dcfe5eeccb3f59cd772f3047ab9c807ea
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-15 16:25:25 +0800

    Update App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from './components/Navbar'
+import MovieHub from './components/MovieHub';
+import MoodMovie from './components/MoodMovie';
 function App() {
   return (
     
-<div className="text-3xl font-bold  text-blue-500">
-      
-
+<div className="text-3xl font-bold ">
       <Navbar/>
+
+<BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path='/MovieHub' element={<MovieHub/>}/>
+          <Route path='/MoodMovie' element={<MoodMovie/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
