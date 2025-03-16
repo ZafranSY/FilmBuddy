dd08e7a12b904388886d4487e7ab9be08354f511
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-16 23:08:35 +0800

    Update App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from './components/Navbar'
 import MovieHub from './components/MovieHub';
 import MoodMovie from './components/MoodMovie';
 import Login from './components/login';
-function App() {
+import SignupPage from './components/SignupPage';
+
+function App() { 
+   const ConditionalNavbar = () => {
+  const location = useLocation(); // Get the current path
+  const excludeNavbarPaths = ['/login','/signup']; // Define paths where the navbar should not appear
+
+  return !excludeNavbarPaths.includes(location.pathname) ? <Navbar /> : null;
+};
   return (
     
 <div className="text-3xl font-bold ">
-      <Navbar/>
 
-<BrowserRouter>
+<BrowserRouter>     {/* Conditionally render the navbar */}
+<ConditionalNavbar />
       <Routes>
         <Route path="/">
           <Route path='/MovieHub' element={<MovieHub/>}/>
           <Route path='/MoodMovie' element={<MoodMovie/>}/>
           <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignupPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
