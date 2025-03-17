1a55528177ffb12f824317916ce6d884baca894c
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-17 22:26:09 +0800

    Create index.ts

---
+++ web-app/src/index.ts
@@ -0,0 +1,9 @@
+import SignupPage from "./components/SignupPage";
+import Login from "./components/login";
+import MovieHub from "./components/MovieHub";
+import MoodMovie from "./components/MoodMovie";
+import Navbar from "./components/Navbar";
+
+export{
+    SignupPage,Login,MovieHub,MoodMovie,Navbar
+}
