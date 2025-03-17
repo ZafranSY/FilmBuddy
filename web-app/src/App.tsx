cd16eabeea27d098bc116022b6a07db0199f8c9f
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-17 20:58:47 +0800

    Update App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-import Navbar from './components/Navbar'
-import MovieHub from './components/MovieHub';
-import MoodMovie from './components/MoodMovie';
-import Login from './components/login';
-import SignupPage from './components/SignupPage';
+import {SignupPage,Login,MoodMovie,MovieHub,Navbar} from'./index'
 
 function App() { 
+  const [token,setToken] = useState(false);
    const ConditionalNavbar = () => {
   const location = useLocation(); // Get the current path
   const excludeNavbarPaths = ['/login','/signup']; // Define paths where the navbar should not appear
 
   return !excludeNavbarPaths.includes(location.pathname) ? <Navbar /> : null;
+
 };
+
+ if(token)
+ {
+  sessionStorage.setItem('token',JSON.stringify(token))
+ }
+ useEffect(()=>{
+  const tokenString = sessionStorage.getItem('token'); // Might return 'string | null'
+
+  if(tokenString){
+    const data = JSON.parse(tokenString )
+    setToken(data)
+    console.log(data); 
+  }
+ })
   return (
     
 <div className="text-3xl font-bold ">
@@ -25,7 +37,7 @@ function App() {
         <Route path="/">
           <Route path='/MovieHub' element={<MovieHub/>}/>
           <Route path='/MoodMovie' element={<MoodMovie/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path='/login' element={<Login setToken={setToken}/>}/>
           <Route path='/signup' element={<SignupPage/>}/>
         </Route>
       </Routes>
